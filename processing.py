5032403016c29f3c9dc9998bb731920e1c0dc08b
Leandra <89750703+lemachadoh@users.noreply.github.com>
2022-05-02 11:09:07 -0300

    Add files via upload

--- processing.py
+++ processing.py
@@ -1,13 +1,18 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-from picamera import PiCamera
+#from picamera import PiCamera
 from time import sleep
 from PIL import Image
 
-
+'''''
 camera = PiCamera()
+camera.start_preview()
+sleep(1)
+camera.rotation = 180
 camera.capture('/home/silicon/Desktop/raspcam.jpg')
+camera.stop_preview()
+
 rasp_cam = Image.open('/home/silicon/Desktop/raspcam.jpg')
 
 
@@ -18,9 +23,14 @@ bottom = 700
 
 img_crop = rasp_cam.crop((left,top,right,bottom))
 img_crop.save('/home/silicon/Desktop/raspcam.jpg')
+'''''
+img = cv.imread('C:\Users\lwmachado\Desktop\fotos\moedas.jpg')
 
+output = img.copy()
+height, width = img.shape[:2]
+maxWidth = int(width/10)
+minWidth = int(width/20)
 
-img = cv.imread('/home/silicon/Desktop/raspcam.jpg')
 
 def color(image):
     image =  cv.cvtColor(image,cv.COLOR_BGR2GRAY)
@@ -41,7 +51,7 @@ def dilatation_and_erosion(threshold):
     return img_erode
 
 
-
+circles = cv.HoughCircles(color(img), cv.HOUGH_GRADIENT, 1.2, 20,param1=50,param2=50,minRadius=minWidth,maxRadius=maxWidth)
 img = dilatation_and_erosion(img)
 
 
@@ -54,8 +64,21 @@ labeled_img = cv.merge([label_hue, blank_ch, blank_ch])
 labeled_img = cv.cvtColor(labeled_img,cv.COLOR_BGR2GRAY)
 labeled_img[label_hue == 0] = 0
 
+
+
+
+if circles is not None:
+    # convert the (x, y) coordinates and radius of the circles to integers
+    circlesRound = np.round(circles[0, :]).astype("int")
+    # loop over the (x, y) coordinates and radius of the circles
+    for (x, y, r) in circlesRound:
+        cv.circle(output, (x, y), r, (0, 255, 0), 4)
+    cv.imwrite(filename = 'test.circleDraw.png', img = output)
+    cv.imwrite(filename = 'test.circleDrawGray.png')
+else:
+    print ('No circles found')
+
 plt.imshow(labeled_img)
 plt.show()
 print('objects number is:', ret-1)
-
 
