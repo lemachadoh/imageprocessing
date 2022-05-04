c17110ab97e733aae8e2538887378d231dd45edc
Leandra <89750703+lemachadoh@users.noreply.github.com>
2022-05-04 10:37:11 -0300

    Add files via upload

--- processing.py
+++ processing.py
@@ -24,12 +24,19 @@ bottom = 700
 img_crop = rasp_cam.crop((left,top,right,bottom))
 img_crop.save('/home/silicon/Desktop/raspcam.jpg')
 '''''
-img = cv.imread('C:\Users\lwmachado\Desktop\fotos\moedas.jpg')
+img = cv.imread(r'C:\Users\lwmachado\Desktop\fotos\moedas.jpg')
 
 output = img.copy()
 height, width = img.shape[:2]
 maxWidth = int(width/10)
 minWidth = int(width/20)
+
+
+minDist = 35
+param1 = 600
+param2 = 100
+minRadius = 0
+maxRadius = 0
 
 
 def color(image):
@@ -39,7 +46,7 @@ def color(image):
 def gamma_and_threshold(image):
     blur = cv.medianBlur(color(image),7)
     gamma = np.array(255* (blur/255)**0.15,dtype='uint8')
-    threshold = cv.adaptiveThreshold(gamma,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,601,10)
+    threshold = cv.adaptiveThreshold(gamma,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,201,1)
     threshold = cv.bitwise_not(threshold)
     return threshold
 
@@ -51,11 +58,18 @@ def dilatation_and_erosion(threshold):
     return img_erode
 
 
-circles = cv.HoughCircles(color(img), cv.HOUGH_GRADIENT, 1.2, 20,param1=50,param2=50,minRadius=minWidth,maxRadius=maxWidth)
-img = dilatation_and_erosion(img)
+circles = cv.HoughCircles(color(img), cv.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
+imagem = dilatation_and_erosion(img)
 
+if circles is not None:
 
-ret, labels = cv.connectedComponents(img)
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        cv.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    else: 
+        print("No circles found")
+
+ret, labels = cv.connectedComponents(imagem)
 
 label_hue = np.uint8(179 * labels / np.max(labels))
 blank_ch = 255 * np.ones_like(label_hue)
@@ -64,21 +78,15 @@ labeled_img = cv.merge([label_hue, blank_ch, blank_ch])
 labeled_img = cv.cvtColor(labeled_img,cv.COLOR_BGR2GRAY)
 labeled_img[label_hue == 0] = 0
 
+shape_circle = circles.shape[1:2:]
 
 
-
-if circles is not None:
-    # convert the (x, y) coordinates and radius of the circles to integers
-    circlesRound = np.round(circles[0, :]).astype("int")
-    # loop over the (x, y) coordinates and radius of the circles
-    for (x, y, r) in circlesRound:
-        cv.circle(output, (x, y), r, (0, 255, 0), 4)
-    cv.imwrite(filename = 'test.circleDraw.png', img = output)
-    cv.imwrite(filename = 'test.circleDrawGray.png')
-else:
-    print ('No circles found')
-
-plt.imshow(labeled_img)
+#plt.imshow(labeled_img)
+plt.imshow(img)
+plt.legend()
+plt.title("Circles counted: ".format(shape_circle))
 plt.show()
 print('objects number is:', ret-1)
+print(circles.shape[1:2])
+
 
